bcbb0d50b09db83b1e3b96dafa804d3deeb66dcb
Devin Gibson <dgib21@hotmail.com>
2020-08-19 18:05:36 -0500

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //displays HTML element as an object known as a DOM element 
 //console.dir(window.document);
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 
 //logging the dom elemnt of the button 
 //console.log(buttonEl);
@@ -18,17 +18,17 @@ var buttonEl = document.querySelector("#save-task");
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 //let us dynamically create the tasks
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
 
-buttonEl.addEventListener("click", createTaskHandler); {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-};
+    console.log(event); 
 
+  };
+
+  formEl.addEventListener("submit", createTaskHandler);
